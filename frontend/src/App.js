fe3173df6b0ddd43a077fa1e902835496cb51caf
Lorenz Keefer <clemenzkafer@gmail.com>
2023-07-03 16:15:09 +0200

    added react app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import data from '../../data/Streaming_History_Audio_2015-2016_0.json';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
